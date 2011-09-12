5225d406ae3ed229fbefd161effa95e611b97a25
Eric Rochester <err8n@virginia.edu>
2011-09-12 14:45:25 -0400

    Remove comments left from previous calls.

--- exercise-001.rb
+++ exercise-001.rb
@@ -1,18 +1,9 @@
 #!/usr/bin/env ruby
 
-# This opens jabberwocky.txt, reads each line, and breaks it into words, which
-# it prints to the screen.
-
-# First, set the name of the file to the constant INPUT. This makes it easy to
-# change the name later.
 INPUT = 'jabberwocky.txt'
 
-# Now, open the file. This executes the block (do |f| ... end), with 'f'
-# assigned to the File object. When the block is done, the file is
-# automatically closed.
 File.open(INPUT) do |f|
 
-  # This executes the block (do |line| ... end) for every line in the file.
   f.each do |line|
 
     # Last time, we printed everything. This time, we'll split each line on
--- exercise-002.rb
+++ exercise-002.rb
@@ -1,22 +1,11 @@
 #!/usr/bin/env ruby
 
-# This opens jabberwocky.txt, reads each line, and breaks it into words, which
-# it prints to the screen.
-
-# First, set the name of the file to the constant INPUT. This makes it easy to
-# change the name later.
 INPUT = 'jabberwocky.txt'
 
-# Now, open the file. This executes the block (do |f| ... end), with 'f'
-# assigned to the File object. When the block is done, the file is
-# automatically closed.
 File.open(INPUT) do |f|
 
-  # This executes the block (do |line| ... end) for every line in the file.
   f.each do |line|
 
-    # When we split on words, it also included the punctuation. We'd like to
-    # get rid of all that.
     line.split.each do |word|
 
       # We'll do this using a very simple regular expression. \w matches
